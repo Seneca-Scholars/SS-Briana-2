6f03c27f00d641da8e4ad3d59694d09815858ee5
Briana Davila <brianadavila@Brianas-MacBook-Pro.local>
2024-08-19 14:30:36 -0500

    updates

--- front-end-project/src/App.jsx
+++ front-end-project/src/App.jsx
@@ -1,50 +1,68 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
-import { useEffect } from 'react';
 
 
 function App() {
-
-
+  //state variables + the functions to update w/ initial value
+  const [data, setData] = useState([]);
+  const [error, setError] = useState (null);
   
+  
+  useEffect(() => {
+    //async so the data is there before state is updated
+    const fetchData = async () => {
+      //url w/ the data being used
+      const url = 'https://jsonplaceholder.typicode.com/users';
 
-  return (
-    <div className="app">
-      <header className="app-header">
-        Next Project
-      </header>
-      <body>
-      <div className="main">
-        <table id="">
-          <thead>
-            <tr>
-              <th>Numbers</th>
-              <th>Letters</th>
+      try {
+        //requests data from url + waits for response
+        const response = await fetch(url);
+
+        //checks if response is successful
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+
+        //changes response to json
+        const result = await response.json();
+        //updates data to json
+        setData(result);
+
+        //handles any errors + shows in console + updates error
+      } catch (error) {
+        console.log ('error')
+        setError(error);
+      }
+    }
+  //calls function
+  fetchData ();
+}, []) //runs once
+
+
+
+return (
+  <div className="app">
+    <div className="main">
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+        <tbody>
+{/*makes rows + columns for each object*/}
+          {data.map(user => (
+            <tr key={user.id}>
+              <td>{user.name}</td>
+              <td>{user.email}</td>
             </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>1234</td>
-              <td>abcd</td>
-            </tr>
-            <tr>
-              <td>1234</td>
-              <td>abcd</td>
-            </tr>
-            <tr>
-              <td>1234</td>
-              <td>abcd</td>
-            </tr>
-            <tr>
-              <td>1234</td>
-              <td>abcd</td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-      </body>
+          ))}
+        </tbody>
+      </table>
     </div>
-  );
+  </div>
+);
 }
 
 
